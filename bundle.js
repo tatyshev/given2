fab926a710d0deae8fd6d7ab7750811b305ac9ef
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-02-24 21:53:12 +0300

    update bundle

--- bundle.js
+++ bundle.js
@@ -7,7 +7,7 @@
 		exports["given"] = factory();
 	else
 		root["given"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
